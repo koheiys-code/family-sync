523ff148d577d9e5be56b2e1e3da797663ea82c1
koheiys-code <kohei7.22wimper@gmail.com>
2026-05-10 10:02:37 +0900

    20260510

--- app.py
+++ app.py
@@ -70,9 +70,9 @@ def apply_edits(expense_manager, sheet_name, edited_df, edit_type):
 
 @st.dialog('納入額計算')
 def calc_monthly_payment(cost_sum, ratio=PAYMENT_RATIO):
-    salary = st.number_input('手取り給料')
+    salary = st.number_input('手取り給料', value=int)
     monthly_payment = salary * ratio - cost_sum
-    st.write(f'{salary}☓{ratio}-{cost_sum}={monthly_payment}')
+    st.write(f'{salary:,}☓{ratio}-{cost_sum:,}={monthly_payment}円')
 
 
 # ユーザー設定の読み込み
