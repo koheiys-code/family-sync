3524dac186af7a2c7906bbe7ebde6cf11e632d93
koheiys-code <kohei7.22wimper@gmail.com>
2026-05-11 22:06:14 +0900

    20260511

--- app.py
+++ app.py
@@ -222,7 +222,7 @@ elif st.session_state['authentication_status']:
                 st.dataframe(decorate_df, hide_index=True)
             else:
                 user_key = name
-                col1, _, col2, col3 = st.columns([1, 4, 1, 1])
+                col1, _, col2, col3 = st.columns([3, 2, 1, 1])
                 with col1:
                     if st.button(f'{user_key}の納入額を計算'):
                         cost_sum = lend_managers_dict[user_key].cost_sum
