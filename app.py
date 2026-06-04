5abc9a77a33fbfcce4f1f8935cba4712cce45420
koheiys-code <kohei7.22wimper@gmail.com>
2026-06-04 19:28:16 +0900

    20260604

--- app.py
+++ app.py
@@ -235,16 +235,17 @@ elif st.session_state['authentication_status']:
                     calc_monthly_payment(user_LM, EM)
                 if st.button('追加', key=f'{name}_add_lend'):
                     add_lend(LM, EM)
-                delete_mode = st.toggle('消去', key='delete_mode')
-                if not delete_mode:
-                    st.dataframe(decorate_df, hide_index=True)
-                else:
-                    deletable_df = decorate_df.copy()
-                    disabled = deletable_df.keys()
-                    deletable_df['消去'] = False
-                    deletable_df = st.data_editor(deletable_df, disabled=disabled, hide_index=True)
-                    if st.button('消去'):
-                        apply_delete(LM, deletable_df)
+                if not decorate_df.empty:
+                    delete_mode = st.toggle('消去', key='delete_mode')
+                    if not delete_mode:
+                        st.dataframe(decorate_df, hide_index=True)
+                    else:
+                        deletable_df = decorate_df.copy()
+                        disabled = deletable_df.keys()
+                        deletable_df['消去'] = False
+                        deletable_df = st.data_editor(deletable_df, disabled=disabled, hide_index=True)
+                        if st.button('消去'):
+                            apply_delete(LM, deletable_df)
 
     with upload_tab:
         with st.form('利用履歴更新フォーム', clear_on_submit=True):
