5ce0b7cd2d1750a417c31a95ea98f65278b9fd3f
koheiys-code <kohei7.22wimper@gmail.com>
2026-06-07 22:58:01 +0900

    20260607

--- finance_manager.py
+++ finance_manager.py
@@ -500,6 +500,7 @@ class ExpensesManager(Manager):
             ax.set_title(f'残高の推移', fontsize=13, pad=15)
             ax.set_xlabel('日付')
             ax.set_ylabel('金額 (円)')
+            ax.yaxis.set_visible(False)
             ax.set_ylim(bottom=0)
             fig.autofmt_xdate()
             plot_dict[account_name] = fig
